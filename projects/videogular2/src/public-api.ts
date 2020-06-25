1fb88cf8472e90e71d29f59e36eafb341cc7932f
Betalos Oralyos <thoura.oussama@gmail.com>
2020-06-25 14:32:56 +0100

    update to support angular cli lib

---
+++ projects/videogular2/src/public-api.ts
@@ -0,0 +1,58 @@
+/*
+ * Public API Surface of videogular2
+ */
+
+// buffering module
+export * from "./buffering/vg-buffering";
+export * from "./buffering/buffering";
+
+// controls module
+export * from "./controls/vg-controls";
+export * from "./controls/vg-fullscreen/vg-fullscreen";
+export * from "./controls/vg-mute/vg-mute";
+export * from "./controls/vg-volume/vg-volume";
+export * from "./controls/vg-play-pause/vg-play-pause";
+export * from "./controls/vg-playback-button/vg-playback-button";
+export * from "./controls/vg-scrub-bar/vg-scrub-bar";
+export * from "./controls/vg-scrub-bar/vg-scrub-bar-buffering-time/vg-scrub-bar-buffering-time";
+export * from "./controls/vg-scrub-bar/vg-scrub-bar-cue-points/vg-scrub-bar-cue-points";
+export * from "./controls/vg-scrub-bar/vg-scrub-bar-current-time/vg-scrub-bar-current-time";
+export * from "./controls/vg-time-display/vg-time-display";
+export * from "./controls/vg-track-selector/vg-track-selector";
+export * from "./controls/vg-quality-selector/vg-quality-selector";
+export * from "./core/services/vg-controls-hidden";
+export * from "./controls/controls";
+
+// core module
+export * from './core/vg-player/vg-player';
+export * from './core/vg-media/vg-media';
+export * from './core/vg-cue-points/vg-cue-points';
+export * from './core/services/vg-api';
+export * from './core/services/vg-fullscreen-api';
+export * from './core/services/vg-utils';
+export * from './core/services/vg-controls-hidden';
+export * from './core/vg-player/vg-player';
+export * from './core/vg-media/vg-media';
+export * from './core/vg-cue-points/vg-cue-points';
+export * from './core/services/vg-api';
+export * from './core/services/vg-fullscreen-api';
+export * from './core/services/vg-utils';
+export * from './core/services/vg-controls-hidden';
+export * from './core/events/vg-events';
+export * from './core/states/vg-states';
+export * from './core/vg-media/i-media-element';
+export * from './core/vg-media/vg-media-element';
+export * from "./core/core";
+
+// ima ads module
+export * from "./ima-ads/vg-ima-ads";
+export * from "./ima-ads/ima-ads";
+
+// overlay play module
+export * from "./overlay-play/vg-overlay-play";
+export * from "./overlay-play/overlay-play";
+
+// stream module
+export * from "./streaming/vg-dash/vg-dash";
+export * from "./streaming/vg-hls/vg-hls";
+export * from "./streaming/streaming";
